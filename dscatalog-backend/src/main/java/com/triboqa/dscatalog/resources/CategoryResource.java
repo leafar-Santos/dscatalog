174bfe4ba54200f6fac3bd8f76b9c441b36be3b9
leafar-Santos <leafarccomputacao@gmail.com>
2024-03-21 18:49:28 -0300

    criação de arquivo de dados mockados para popular a base de dados

--- dscatalog-backend/src/main/java/com/triboqa/dscatalog/resources/CategoryResource.java
+++ dscatalog-backend/src/main/java/com/triboqa/dscatalog/resources/CategoryResource.java
@@ -1,6 +1,5 @@
 package com.triboqa.dscatalog.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
--- dscatalog-backend/src/main/java/com/triboqa/dscatalog/services/CategoryService.java
+++ dscatalog-backend/src/main/java/com/triboqa/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.triboqa.dscatalog.entities.Category;
 import com.triboqa.dscatalog.repositories.CategoryRepository;
@@ -16,6 +17,7 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository categoryRepository;
 	
+	@Transactional(readOnly = true) //Evita travar o banco para operações quando faz leitura de dados
 	public List<Category> listaTodasCategorias(){
 		return categoryRepository.findAll();
 		
